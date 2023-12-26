3bd52a75fe4d911af4e8d4bdd71256c1c064beac
Maksym Bozhko <maksym.bozhko@niko-tech.eu>
2023-12-26 11:36:59 +0200

    add counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { Card } from "./components/Card/Card";
-import { courses } from "./data/data";
+// import { Card } from "./components/Card/Card";
+// import { courses } from "./data/data";
+import Counter from "./components/Counter/Counter";
 
 function App() {
   return (
     <div className="App">
-      <Card courses={courses}></Card>
+      {/* <Card courses={courses}></Card> */}
+      <Counter></Counter>
     </div>
   );
 }
