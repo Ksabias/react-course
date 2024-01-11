4bad6554f908b75d36b2699bccd2bc18842c8874
Maksym Bozhko <maksym.bozhko@niko-tech.eu>
2024-01-11 23:16:53 +0200

    fix for empty context obj

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -4,13 +4,17 @@ import { UserInfoContext } from "../../App";
 const UserCard = () => {
   const data = useContext(UserInfoContext);
 
+  if (!data || Object.entries(data)?.length === 0) {
+    return <h1>No data yet</h1>;
+  }
+
   return (
     <div>
       <h1>User info</h1>
 
       <div>Username: {data.name}</div>
 
-      {data?.permissions?.length > 0 && (
+      {data.permissions?.length > 0 && (
         <div>
           Permissions:
           {data.permissions?.map((permission, index) => (
